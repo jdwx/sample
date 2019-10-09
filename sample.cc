3fbce82f73f72979184e0238cb8726816c03256c
jdw <jdw@NearlyFreeSpeech.NET>
2019-10-09 22:47:06 +0000

    Comments, because why not?

--- sample.cc
+++ sample.cc
@@ -27,6 +27,7 @@ int main( int argc, char const * argv[] ) {
 	std::vector<unsigned>    vIn;
 	std::vector<unsigned>    vOut;
 
+	//  Parse arguments.
 	if ( argc < 3 || argc > 4 )
 		Usage( argv[ 0 ] );
 
@@ -37,29 +38,29 @@ int main( int argc, char const * argv[] ) {
 		argNext += 1;
 	}
 
-
 	std::ifstream ifsFile( argv[ argNext++ ] );
 	linesOut = static_cast< unsigned >( std::stoul( argv[ argNext ] ) );
 	if ( 0 == linesOut )
 		Usage( argv[ 0 ] );
 
+	//  Optionally save header.
 	if ( bHeader )
 		std::getline( ifsFile, stHeader );
 
+	//  Read whole file into memory (sorry Nagesh ;-) ) and count the lines.
 	while ( std::getline( ifsFile, st ) ) {
 		vLines.emplace_back( std::move( st ) );
 		vIn.emplace_back( linesIn++ );
 	}
 
+	//  Validate the requested number of samples.
 	if ( linesOut > linesIn ) {
 		cerr << "Requested " << linesOut << " samples but only "
 			<< linesIn << " candidates were found." << endl;
 		return 20;
 	}
 
-	if ( bHeader )
-		cout << stHeader << endl;
-
+	//  Choose lines from the input at random (using the Knuth shuffle).
 	for ( unsigned u = 0 ; u < linesOut ; u += 1 ) {
 		unsigned uLine = mt() % linesIn;
 		vOut.push_back( vIn[ uLine ] );
@@ -67,7 +68,12 @@ int main( int argc, char const * argv[] ) {
 		std::iter_swap( vIn.begin() + uLine, vIn.begin() + linesIn );
 	}
 
+	//  Put the selected lines back in the original order.
 	std::sort( vOut.begin(), vOut.end() );
+
+	//  Write the output (including optional header).
+	if ( bHeader )
+		cout << stHeader << endl;
 
 	for ( auto it : vOut )
 		cout << vLines[ it ] << endl;
